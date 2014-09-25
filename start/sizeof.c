72b460bd3a081d57f3c6ae2f9ea25ec11eaf9ded
GowthamVasishta <gowtham.vasista@gmail.com>
2014-09-25 23:15:11 +0530

    sizeof keyword

---
+++ start/sizeof.c
@@ -0,0 +1,11 @@
+/*
+ * usage of 'sizeof' keyword
+ */
+
+#include<stdio.h>
+void main() {
+	printf("In linux operating system, \n");
+	printf("size of int = %d \n", sizeof(int));
+	printf("size of char = %d \n", sizeof(char));
+	printf("size of float = %d \n", sizeof(float));
+}

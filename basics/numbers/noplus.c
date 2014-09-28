6bba4db18153a2f8c1c4ddca0700362a303189c2
GowthamVasishta <gowtham.vasista@gmail.com>
2014-09-28 13:30:05 +0530

    entered correct formula

--- basics/numbers/noplus.c
+++ basics/numbers/noplus.c
@@ -1,4 +1,7 @@
-/* Write a C program to add two numbers without using plus '+' operator. */
+/*
+ * Write a C program to add two numbers without using plus '+' operator.
+ *
+ */
 
 #include<stdio.h>
 void main() {
@@ -8,7 +11,7 @@ void main() {
    scanf("%d %d", &i, &j);
   
    // '~' is 1's complement operator
-   // ~x = -x +1
+   // -x = ~x +1
    k = i - ~j -1;
 
    printf("The sum is %d \n", k); 
